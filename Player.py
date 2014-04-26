2577d8fc6e628a6fcc5c3a19abd01e92cc4717e9
Antoine <antoineff@gmail.com>
2014-04-26 20:42:35 +0200

    Vitesse entammée

--- Player.py
+++ Player.py
@@ -1,12 +1,59 @@
-class Player:
+class Player(GameEntity):
     BASECOLOR = pygame.Color('darkred')
+	BASE_SPEED = 2
     SIZE = 16
 
-    def __init__(self, x, y ):
+    def __init__(self, x_game, y_game ):
         # TODO: ajouter vitesse , fonctions de saut et de déplacement
-        self.x, self.y = x,y
+        self.x_game, self.y_game = x_game,y_game
+        self.x_screen = x_game
+		self.y_screen = 
+		self.vx = 0
+		self.vy = 0
+		self.in_air = False
+		self.time_in_air = 0
         self.environ = DynamicLevel()
-        self.img = 
+        # self.img = 
+
+	def startMovingRight(self):
+		self.moving_right = True
+
+	def startMovingLeft(self):
+		self.moving_left = True
+
+	def stopMoving(self):
+		'''self.moving_right = False # on verifie la valeur avant ?
+		self.moving_left = False'''
+
+	def updatePosition(self):
+		if (self.begin_jump):
+			self.vy = Player.BASE_SPEED - self.time_in_air
+
+		if not (self.moving_right or self.moving_left):
+			self.vx = 0.
+		else:
+			if self.moving_right:
+				self.vx = Player.BASE_SPEED
+			if self.moving_left:
+				self.vx = -Player.BASE_SPEED
+
+		if (self.#valeur a recuperer du decor):
+			self.in_air = False
+			self.time_in_air = 0
+
+		if (self.in_air):
+			self.x_game += self.vx * self.time_in_air
+			self.x_screen = self.x_game
+			self.y_game += self.vy * self.time_in_air - ((self.time_in_air)^2)/2
+			self.y_screen = self.y_game
+			self.time_in_air += 1
+		else:
+			self.x_game += self.vx
+			self.x_screen = self.x_game
+
+	def startJumping(self):
+		self.begin_jump = True
+		self.in_air = True
 
     def test(self):
         print "yoyo"
