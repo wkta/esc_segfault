f04c99777add2b9e1c8ad40cf119f09feb14a640
Thomas Iwaszko <tom@asus-laptop.(none)>
2014-04-27 16:19:36 +0200

    ajout ecran intro permettant dafficher du texte pr le joueur

--- Game.py
+++ Game.py
@@ -4,6 +4,8 @@ import pygame
 from Player import Player
 from SkillBar import SkillBar 
 from time import sleep
+
+from  IntroScreen import IntroScreen 
 from DynamicLevel import DynamicLevel
 from PitfallLevel import PitfallLevel
 
@@ -24,11 +26,11 @@ class Game:
     def __init__(self):
         pygame.init()
         Game.pl = Player( 0, 0 )  #TODO :  position realiste
-        #Game.pl.setEnviron( DynamicLevel()  )
+        Game.pl.setEnviron( IntroScreen("hoj hoj hoj")  )
+        self.state = Game.ST_INTRO
 
         Game.sk_bar = SkillBar( Game.pl )
         Game.window = pygame.display.set_mode( (DISP_WIDTH, DISP_HEIGHT) )
-        self.state = Game.ST_PLATEFORMER
         self.x_plat_list = list()
         self.y_plat_list = list()
         self.affich_new_plat = True
@@ -44,26 +46,28 @@ class Game:
         prog_done = False
         last_ref = pygame.time.get_ticks()
         self.refreshScreen()
-        self.x_plat_list = Game.pl.environ.x_plat_list
-        self.y_plat_list = Game.pl.environ.y_plat_list
+        self.x_plat_list = None
+        self.y_plat_list = None
         
         while not prog_done:
-            self.x_plat_list = Game.pl.environ.x_plat_list
-            self.y_plat_list = Game.pl.environ.y_plat_list
-        
-            if (abs(self.y_last_affich - Game.pl.y_game)>(600/2)):
-                self.affich_new_plat = True
-            if (self.affich_new_plat):
-                #print(len(Game.pl.environ.entity_list))
-                Game.pl.environ.removeAllPlatform()
-                #print(len(Game.pl.environ.entity_list))
-                for y_plat in self.y_plat_list:
+            #gestion des plateformes
+            if(self.state==Game.ST_PLATEFORMER):
+                self.x_plat_list = Game.pl.environ.x_plat_list
+                self.y_plat_list = Game.pl.environ.y_plat_list
+                if (abs(self.y_last_affich - Game.pl.y_game)>(600/2)):
+                    self.affich_new_plat = True
+                if (self.affich_new_plat):
                     #print(len(Game.pl.environ.entity_list))
-                    if (abs(y_plat - Game.pl.y_game)<(600)):
-                        Game.pl.environ.generatePlatform(self.x_plat_list[self.y_plat_list.index(y_plat)], y_plat)
-                self.affich_new_plat = False
-                self.y_last_affich = Game.pl.y_game
-                #print(len(Game.pl.environ.entity_list))
+                    Game.pl.environ.removeAllPlatform()
+                    #print(len(Game.pl.environ.entity_list))
+                    for y_plat in self.y_plat_list:
+                        #print(len(Game.pl.environ.entity_list))
+                        if (abs(y_plat - Game.pl.y_game)<(600)):
+                            Game.pl.environ.generatePlatform(self.x_plat_list[self.y_plat_list.index(y_plat)], y_plat)
+                    self.affich_new_plat = False
+                    self.y_last_affich = Game.pl.y_game
+                    #print(len(Game.pl.environ.entity_list))
+            #fin gestion plateformes
                     
             for event in pygame.event.get(): 
                 if event.type == pygame.QUIT: 
@@ -85,6 +89,11 @@ class Game:
                         Game.pl.stopMoving()
                     if event.key == pygame.K_LEFT:
                         Game.pl.stopMoving()
+                    if event.key == pygame.K_RETURN:
+                        if(  isinstance(Game.pl.environ, IntroScreen )):
+                            del Game.pl.environ
+                            Game.pl.setEnviron( PitfallLevel() )
+                            self.state=Game.ST_GAME_FALL
 
             if(  isinstance(Game.pl.environ, PitfallLevel)):
                 self.state = Game.ST_GAME_FALL
@@ -102,7 +111,7 @@ class Game:
 
     def refreshScreen(self ):
         Game.pl.environ.markToDisplay( Game.window )
-        if(not self.state==Game.ST_GAME_FALL ):
+        if( self.state==Game.ST_PLATEFORMER ):
             Game.sk_bar.markToDisplay(Game.window)
             Game.pl.markToDisplay( Game.window ) 
         # TODO cest le joueur qui pourrait declencher le declencehement du dessin du terrain
