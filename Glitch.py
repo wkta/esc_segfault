549ab0bcd56045ce37f4884010d84d0ee41ee91a
AceyT <thiry.alexandre@gmail.com>
2014-04-27 05:28:49 +0200

    Getters et Setters de MobileEnt fini (à confirmer) et début de classe de Glitch avec fonction d'update automatique pour suivre un mouvement défini

---
+++ Glitch.py
@@ -0,0 +1,32 @@
+from MobileEnt import MobileEnt
+
+class Glitch(MobileEnt):
+	"""Classe qui permet de generer des glitch et de definir leurs mouvements"""
+
+	def __init__(self, x_game, y_game, x_movement, y_movement):
+		self.set_pos(x_game, y_game)
+		self.set_init_pos(x_game, y_game)
+		self.set_movement(x_movement, y_movement)
+		pass
+
+	def inverse_vector_x(self):
+		vector_x, vector_y = self.get_vect()
+		self.set_vect((-vector_x, vector_y))
+		pass
+
+	def inverse_vector_y(self):
+		vector_x, vector_y = self.get_vect()
+		self.set_vect((vector_x, -vector_y))
+		pass
+
+	#TODO : un mouvement qui ralentit avant d'atteindre la limite et reprends de la vitesse dans l'autre sens si le temps le permet
+	def update_pos(self):
+		if self.get_movement()[0]:
+			if abs(self.get_init_pos()[0] - self.get_pos()[0]) > self.get_movement()[0]:
+				self.inverse_vector_x()
+		if self.get_movement()[1]:
+			if abs(self.get_init_pos()[1] - self.get_pos()[1]) > self.get_movement()[1]:
+				self.inverse_vector_y()
+		self.move()
+
+
--- MobileEnt.py
+++ MobileEnt.py
@@ -1,21 +1,53 @@
 import pygame
+from GameEntity import GameEntity
+from global_vars import *
 
 class MobileEnt(GameEntity):
 	"""Classe de base pour les entites mobiles"""
+
 	def __init__(self):
-		self.x, self.y = 0, 0
+		self.x_game, self.y_game = 0, 0
+		self.__INIT_X, self.__INIT_Y = 0, 0
+		self.w, self.h = 48, 48 #TODO : des tailles variables ? Laisser les sous classes s'en charger et gérer les collisions dedans ?
 		self.vx, self.vy = 0, 0
+		self.x_movement, self.y_movement = 0, 0
 		pass
 
 	def set_pos(self, x, y):
-		self.x, self.y = x, y
+		self.x_game, self.y_game = x, y
 		pass
 
 	def get_pos(self):
-		return(self.x, self.y)
+		return(self.x_game, self.y_game)
 
-	def set_vect(self, vx, vy):
-		self.vx, self.vy = vx, vy
+	def set_vect(self, vector):
+		self.vx, self.vy = vector[0], vector[1]
+		pass
 
 	def get_vect(self):
 		return(self.vx, self.vy)
+
+	def move(self):
+		self.x_game += self.vx
+		self.y_game += self.vy
+		pass
+
+	def set_init_pos(self, init_x, init_y):
+		self.__INIT_X, self.__INIT_Y = init_x, init_y
+		pass
+
+	def get_init_pos(self):
+		return(self.__INIT_X, __INIT_Y)
+
+	def set_movement(self, range_x, range_y):
+		self.x_movement, self.y_movement = range_x, range_y
+		pass
+
+	def get_movement(self):
+		return(self.x_movement, self.y_movement)
+
+	def markToDisplay(self, window, pl_y_game):
+		x_screen, y_screen = game_to_scr_coord( self.x_game, self.y_game, pl_y_game)
+		pygame.draw.rect( window, pygame.Color('RED'),
+			pygame.Rect(x_screen, y_screen, self.w, self.h ))
+		pass
